2f424601337fa9164b96137aee3ad8c306194699
omersheikhh <shaikhomerahmed78@gmail.com>
2024-09-24 02:06:39 +0500

    Nav Updated

--- ecommerce-react-master/src/components/basket/Basket.jsx
+++ ecommerce-react-master/src/components/basket/Basket.jsx
@@ -77,7 +77,7 @@ const Basket = () => {
             type="button"
           >
             Sign in to checkout
-          </button>
+          </button>v
         </div>
       </Modal>
       <div className="basket">
