80a0c0f844e4ce45b894721c7aac9df3313489e5
omersheikhh <shaikhomerahmed78@gmail.com>
2024-09-25 03:25:11 +0500

    Footer Update

--- ecommerce-react-master/src/components/basket/Basket.jsx
+++ ecommerce-react-master/src/components/basket/Basket.jsx
@@ -77,7 +77,7 @@ const Basket = () => {
             type="button"
           >
             Sign in to checkout
-          </button>zvvvvv
+          </button>zvvvvvv
         </div>
       </Modal>
       <div className="basket">
